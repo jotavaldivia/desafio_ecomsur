4b15f4c713a308839cd96d8278a87474fbf92c25
juan <juanvaldebenitovaldivia@outlook.es>
2021-03-26 21:09:56 -0300

    actualizacion js

--- js/script.js
+++ js/script.js
@@ -11,23 +11,47 @@ let getCountry = ()=>{
    fetch(url)
    .then((resp)=> resp.json())
    .then((newData)=>{
-      console.log(newData);
+      
+      
       search_results.innerHTML =  `
                 <span class="search__results__span">Capital </span> <p>${newData[0].capital}<p><br>
                 <span class="search__results__span">Bandera <p class="search__results__p"><img class="search__results__img" src="${newData[0].flag}" width="30%"/></p>
                 <span class="search__results__span">Nombre Completo del País </span><p>${newData[0].altSpellings[2]}</p>
                 <span class="search__results__span">Región </span><p>${newData[0].region}</p>
-                <span class="search__results__span">Países Limístrofes </span><p>${newData[0].borders}</p>
+                <span class="search__results__span">Países Limístrofes </span><div id="search__code"></div>
                 <span class="search__results__span">Lengua </span><p>${newData[0].languages[0].nativeName}</p>
                 <span class="search__results__span">Nombre y Simbolo</span><p>${newData[0].currencies[0].name} :${newData[0].currencies[0].symbol} </p>
       
       
       `
+      getCode(newData[0].borders);
+      
    })
    .catch((e)=>{
       console.log(e); 
    })
 
    }
+
+
+let getCode = (code)=>{
+   code.forEach(element =>{
+      let search_code= document.getElementById("search__code")
+      let urlcode = `https://restcountries.eu/rest/v2/alpha?codes=${element}`
+      fetch(urlcode)
+      .then((resp)=> resp.json())
+      .then((newDataCode)=>{
+         console.log(newDataCode)
+         search_code.innerHTML += `
+         <p>${newDataCode[0].name}</p>
+         `
+         
+      })
+      .catch((e)=>{
+         console.log(e); 
+      })
+
+});
+}
 }
 
